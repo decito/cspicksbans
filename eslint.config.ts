1d3069a1c85e13c9900e2c66c7d0a72fd7f63742
Décio Silva <61292095+decito@users.noreply.github.com>
2026-08-24 19:54:40 -0300

    security: bump dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,46 @@
+import prettierConfig from '@vue/eslint-config-prettier'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import { globalIgnores } from 'eslint/config'
 
 export default defineConfigWithVueTs(
-  { name: 'app/files-to-lint', files: ['**/*.{ts,mts,tsx,vue}'] },
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}']
+  },
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  skipFormatting
+  skipFormatting,
+  prettierConfig,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+      ],
+      'vue/html-self-closing': [
+        'warn',
+        {
+          html: {
+            void: 'always',
+            normal: 'always',
+            component: 'always'
+          }
+        }
+      ],
+      'vue/html-closing-bracket-newline': [
+        'warn',
+        {
+          singleline: 'never',
+          multiline: 'always',
+          selfClosingTag: {
+            singleline: 'never',
+            multiline: 'always'
+          }
+        }
+      ]
+    }
+  }
 )
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
